214439fc06a5ebc050ea0f4ce6e9eeb532568f77
l5211314121 <1045246825@qq.com>
2020-12-27 12:01:35 -0500

    add request data

--- jenkins/models.go
+++ jenkins/models.go
@@ -3,13 +3,12 @@ package jenkins
 import "github.com/bndr/gojenkins"
 
 const (
-	JENKINS_BASE_URL = "http://47.92.148.31:32008/"
-	JENKINS_ADMIN_USER = "yesheng"
-	JENKINS_ADMIN_PASS = "chouxifu.521"
+	JENKINS_BASE_URL   = "http://47.92.148.31:32008/"
+	JENKINS_ADMIN_USER = "xxxx"
+	JENKINS_ADMIN_PASS = "xxxx"
 )
 
 type JenkinsCmd struct {
 	*gojenkins.Jenkins
 	//Reconnect func() *JenkinsCmd
 }
-
